5bce028349af410d676a97f5fe02af84818665da
007Arran <aj41697@gmail.com>
2023-04-15 15:42:00 -0600

    Initial Commit

---
+++ The Daily Stretch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  The Daily Stretch
+//
+//  Created by Arran James on 4/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ The Daily Stretch/The_Daily_StretchApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Daily_StretchApp.swift
+//  The Daily Stretch
+//
+//  Created by Arran James on 4/15/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Daily_StretchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
